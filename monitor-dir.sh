e25bb1773a4658ecbadd826f69e744135f8b31dc
ken-nas <kenchou77@gmail.com>
2021-12-24 21:13:39 +0800

    monitor multiple dirs

---
+++ monitor-dir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ -z $1 ]]; then
+    echo "Usage:" $(basename $0) "[PATH...]"
+    exit 1
+fi
+
+BIN_PATH=$(dirname $0)
+inotifywait -me moved_to "$@" | while read dir action file; do
+    echo "The file '$file' appeared in directory '$dir' via '$action'"
+    # do something with the file
+    ${BIN_PATH}/cleanup.py -vvv --prune "${dir}/${file}"
+done
+
